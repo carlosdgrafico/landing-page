6f5d93e35625b87c0e6ecd0d39384284a8ecf225
carlosdgrafico <carlos.disenadorgrafico@gmail.com>
2017-09-25 15:22:57 -0300

    seccion partners y carousel

--- app/js/wisegrowth.js
+++ app/js/wisegrowth.js
@@ -1,6 +1,27 @@
 $(document).ready(function(){
 
-
+   // slider para la seccion de partners
+      $('.owl-partners').owlCarousel({
+         // animateOut: 'slideOutDown',
+         // animateIn: 'flipInX',
+   		loop: true,
+   		autoplay: true,
+   		margin: 10,
+   		nav: true,
+   		dots: true,
+   		navText: ["<div class='prev'></div>", "<div class='next'></div>"],
+   		responsive: {
+   			0: {
+   				items: 1
+   			},
+   			600: {
+   				items: 1
+   			},
+   			1000: {
+   				items: 1
+   			}
+   		}
+   	});
 
 
 });
